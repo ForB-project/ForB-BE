719ad3a320e91d3d91234be1aeb50fe1d38fcd82
sohyuneeee <sohyuney@naver.com>
2022-09-19 10:47:04 +0900

    initial commit

---
+++ src/main/java/com/innovationcamp/finalprojectforb/FinalProjectForBApplication.java
@@ -0,0 +1,13 @@
+package com.innovationcamp.finalprojectforb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectForBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectForBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innovationcamp/finalprojectforb/FinalProjectForBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovationcamp.finalprojectforb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectForBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
