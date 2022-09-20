5d51c9f977dc1562ba04da8538773cec9e49d716
sohyuneeee <sohyuney@naver.com>
2022-09-20 13:29:55 +0900

    깃 이그노어 추가, 어노테이션 수정

--- src/main/java/com/innovationcamp/finalprojectforb/jwt/TokenProvider.java
+++ src/main/java/com/innovationcamp/finalprojectforb/jwt/TokenProvider.java
@@ -17,7 +17,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.Date;
 import java.util.Optional;
