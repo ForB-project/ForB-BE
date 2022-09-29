89d8a2fe388b9f7bd4fc072cdca01091f5bd0375
sejin\admin <seijin0722@naver.com>
2022-09-29 19:06:21 +0900

    feature : 콘텐츠 get 방식에 좋아요 개수 반영

--- src/main/java/com/innovationcamp/finalprojectforb/repository/HeartRepository.java
+++ src/main/java/com/innovationcamp/finalprojectforb/repository/HeartRepository.java
@@ -9,4 +9,6 @@ public interface HeartRepository  extends JpaRepository<Heart, Long> {
     // 동일한 콘텐츠에 동일한 계정으로 이미 좋아요한 내역이 있는지 찾을 때 사용할 메소드
     List<Heart> findByMemberIdAndContentId(Long member, Long contentId);
 
+    Long countHeartByContentId(Long contentId);
+
 }
