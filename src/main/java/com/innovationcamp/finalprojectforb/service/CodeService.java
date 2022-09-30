b41b7a548c087eb8a485e7eaa3e3dab99631f5d8
sejin\admin <seijin0722@naver.com>
2022-09-30 20:11:45 +0900

    modify : 코드체험 입력 값 별 사용자 경험 추가 (프론트 요청)

--- src/main/java/com/innovationcamp/finalprojectforb/service/CodeService.java
+++ src/main/java/com/innovationcamp/finalprojectforb/service/CodeService.java
@@ -21,15 +21,24 @@ public class CodeService {
     public ResponseDto<String> showSecondCode(CodeRequestDto codeRequestDto) {
         String finalAnswer = "";
         int treeHit =  0;
-        int howMany = codeRequestDto.getInputInt1();
-            while (treeHit < howMany) {
-                if (howMany >= 21) {
+        int firstInput = codeRequestDto.getInputInt1();
+        int secondInput = codeRequestDto.getInputInt2();
+            while (treeHit < firstInput) {
+                if (firstInput >= 21 && secondInput >= 21) {
                     finalAnswer = "20이하로 설정해주세요!";
+                    break;
+                }
+                if (firstInput > secondInput) {
+                    finalAnswer = "이미 넘어간 나무를 계속 찍을 수 없습니다!";
+                    break;
+                }
+                if (firstInput < secondInput) {
+                    finalAnswer = "나무를 넘어뜨릴 타이밍을 생각해보세요!";
                     break;
                 }
                 treeHit++;
                 finalAnswer += "나무를 " + treeHit + "번 찍었습니다.";
-                if (treeHit == howMany) {
+                if (treeHit == secondInput) {
                     finalAnswer += "나무 넘어갑니다.";
                 }
             }
