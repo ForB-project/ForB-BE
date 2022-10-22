de2ea1a974014bae0e33a991f662770974d2dc25
sohyuneeee <sohyuney@naver.com>
2022-10-22 12:09:41 +0900

    modify: 테스트 결과 Response 수정(프론트요청) 및 리팩토링

--- src/main/java/com/innovationcamp/finalprojectforb/controller/TestResultController.java
+++ src/main/java/com/innovationcamp/finalprojectforb/controller/TestResultController.java
@@ -2,27 +2,28 @@ package com.innovationcamp.finalprojectforb.controller;
 
 import com.innovationcamp.finalprojectforb.dto.ResponseDto;
 import com.innovationcamp.finalprojectforb.dto.TestResultRequestDto;
-import com.innovationcamp.finalprojectforb.service.TestService;
+import com.innovationcamp.finalprojectforb.service.TestResultService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class TestResultController {
 
-    private final TestService testService;
+    private final TestResultService testResultService;
 
-    //DB에 문항 및 테스트 결과 넣기 (한 번 넣고 주석 처리하기)
-//    @GetMapping("/api/create")
-//    public void create(){
-//        testService.createDB2();
-//    }
+//    DB에 문항 및 테스트 결과 넣기 (한 번 넣고 주석 처리하기)
+    @GetMapping("/api/create")
+    public void create(){
+        testResultService.createDB2();
+    }
 
     @PostMapping("/api/test/result")
     public ResponseDto<?> result(@RequestBody TestResultRequestDto testResultRequestDto){
-        return testService.result(testResultRequestDto);
+        return testResultService.result(testResultRequestDto);
     }
 
 }
